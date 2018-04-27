08932e430f6cd6f7af3b4a50240477effbc52ce0
git <1046396106@qq.com>
2018-04-27 11:52:27 +0800

    python

---
+++ aa.py
@@ -0,0 +1,30 @@
+# 99乘法表
+#  100-200   3...1     4....2    5....3
+#  鸡兔同笼  35头  94只脚
+#  杨辉三角
+'''
+for num in range(100,201):
+    if num%3==1 and num%4==2 and num%5==3:
+        print(num);
+
+
+head=35
+foot=94
+
+for ji in range(1,36):
+    tu=35-ji
+    if ji*2+tu*4==94:
+        print(ji,tu)
+'''
+
+rows=21
+
+for row in range(1,rows):
+    str=""
+    for space in range(1,rows-row):
+        str+=" "
+    for star in range(1,row*2-1+1):
+        str+="*"
+    print(str)
+
+
